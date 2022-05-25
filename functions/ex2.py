138d79883556e37df34bdcd10a2973213c2354c6
Bona Akubue <bonaakubue@gmail.com>
2022-05-25 09:54:41 +0100

    positional arguments in function ex2.py

--- functions/ex2.py
+++ functions/ex2.py
@@ -1,5 +1,12 @@
+#positional arguments
+
 def numbers (a, b, c):
     result = a + b - c
     print(result)
 
+#calling the function - 1
 numbers (5, 3, 1)
+
+#calling the function - 2
+numbers (5, 1, 3)
+
